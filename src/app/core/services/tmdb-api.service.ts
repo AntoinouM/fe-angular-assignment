e690909b0cb20f3428e6a6c5a7980571c6561341
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-06 10:14:14 +0100

    setup of api.service.ts

--- src/app/core/services/tmdb-api.service.ts
+++ src/app/core/services/tmdb-api.service.ts
@@ -1,9 +1,20 @@
+// tmdb-api.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' 
 })
 export class TmdbApiService {
+  private apiUrl = environment.apiUrl;
+  private apiKey = environment.apiKey;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getConfiguration(): Observable<any> {
+    return this.http.get(`${this.apiUrl}?api_key=${this.apiKey}`);
+  }
 }
+
