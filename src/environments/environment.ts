5af5a1082821f4b4a379c584f7577c2a56102457
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-06 11:31:01 +0100

    configuration of environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: process.env["API_URL"],
+    apiUrl: 'https://api.themoviedb.org/3/configuration',
     apiKey: process.env["API_KEY"]
   };
