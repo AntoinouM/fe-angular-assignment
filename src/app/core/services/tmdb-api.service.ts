bd5b69c1fc102f45721c20af2488a192b6a0919a
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-06 15:04:42 +0100

    testing with header

--- src/app/core/services/tmdb-api.service.ts
+++ src/app/core/services/tmdb-api.service.ts
@@ -10,18 +10,14 @@ export class TmdbApiService {
   private endpoint = '';
   private apiUrl = environment.apiUrl;
   private apiKey = environment.apiKey;
-  private options = {
-    method: 'GET',
-    headers: {
+  private headers = {
       accept: 'application/json',
       Authorization: `Bearer ${this.apiKey}`
-    }
   };
 
   constructor(private http:HttpClient) {}
 
   getData() {
-    console.log(import.meta.env.NODE_ENV)
     return this.http.get(this.apiUrl)
   }
 }
