799303ffcc5343fa89cd6308cbf7606d6e328f3f
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-06 15:31:32 +0100

    [init] create basic rounting between Movie and TVSHow tab

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { TvshowsComponentComponent } from './shared/components/tvshows-component/tvshows-component.component';
+import { MoviesComponentComponent } from './shared/components/movies-component/movies-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'App TV Shows',
+        component: TvshowsComponentComponent,
+      },
+      {
+        path: 'movies',
+        title: 'App Movies',
+        component: MoviesComponentComponent,
+      },
+];
