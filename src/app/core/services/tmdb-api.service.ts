e3de4571512625b21183a0dfaa9867ecdbab6c26
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-07 10:51:18 +0100

    [refactor] Refactored the code of the service to match httpHeaders to httpParams
    - creation of a function that return an httpHeaders type

--- src/app/core/services/tmdb-api.service.ts
+++ src/app/core/services/tmdb-api.service.ts
@@ -16,16 +16,25 @@ export class TmdbApiService {
 
   // functions for specific request
   // const url = 'https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1';
+  // const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
   getTopRated(page: number, mediaType: string): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${this.apiKey}`
-    })
+    const headers = this.buildHeaders(this.apiKey);
     const params = this.buildParams({ page: page.toString(), language: this.language })
     return this.http.get(`${this.apiUrl}/${mediaType}/top_rated`, { headers , params })
   }
 
-  // function to generate call parameters
+  // call to specific id
+  getMedia(id: string) {}
+
+
+  // private functions to control request
+  private buildHeaders(key: string): HttpHeaders {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${key}`
+    })
+    return headers;
+  }
   private buildParams(params: any): HttpParams {
     let httpParams = new HttpParams()
     for (const key in params) {
