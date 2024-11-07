cbe86a9b9c13ac9ff36b680db4be4ff4f4350e60
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-07 12:23:44 +0100

    [add] navbar component

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, NgOptimizedImage],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
