7aa18d60e9168ac3848aa8414e106aedafa15b5b
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-07 15:07:36 +0100

    [enhance] navbar responsive
    - added conditional classes
    - added burger menu on mobile view

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,14 +1,24 @@
-import { NgOptimizedImage } from '@angular/common';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NgOptimizedImage],
+  imports: [RouterOutlet, RouterLink, NgOptimizedImage, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  
+
+  isMobileMenuOpen: boolean = false;
+  isSearchActive: boolean = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    console.log(this.isMobileMenuOpen)
+  }
 
 }
