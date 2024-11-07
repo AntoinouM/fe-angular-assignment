f146b8cb2690357a3be11acc743f0b4b105605af
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-07 16:47:59 +0100

    [addition] spinner while 1s debounce on search

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { NgClass, NgOptimizedImage } from '@angular/common';
-import { RouterOutlet, RouterLink, ActivatedRoute } from '@angular/router';
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, filter, tap } from 'rxjs';
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NgOptimizedImage, NgClass],
+  imports: [RouterOutlet, RouterLink, NgOptimizedImage, NgClass, ReactiveFormsModule, SpinnerComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -13,9 +16,36 @@ export class NavbarComponent {
 
   isMobileMenuOpen: boolean = false;
   isSearchActive: boolean = false;
+  isSearchLoading: boolean = false;
   query: string = '';
+  searchControl = new FormControl('');
 
-  inputElement = viewChild<ElementRef>('input');
+  constructor() {
+    this.initializeSearch();
+  }
+
+  initializeSearch() {
+    this.searchControl.valueChanges
+      .pipe(
+        tap((value: string | null) => {
+          this.isSearchLoading = !!value && value.length >= 3;
+        }),
+        filter((value): value is string => value !== null),
+        debounceTime(1000),
+        filter((value: string) => value.length >= 3),
+        tap(() => {
+          this.isSearchLoading = false;
+        })
+      )
+      .subscribe((searchTerm) => {
+        this.query = searchTerm;
+        this.performSearch(searchTerm);
+      });
+  }
+
+  performSearch(searchTerm: string) {
+    console.log('Search triggered with:', searchTerm);
+  }
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
