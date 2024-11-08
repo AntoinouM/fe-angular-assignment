19021929f0faf13e7f2117e247dc35ea28071516
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-08 15:39:49 +0100

    [addition] add function, interface and logic
    - create data fetching logic for tvshow-detailed component
    - add getCredits to get cast info
    - add actor interface for future display

---
+++ src/app/core/models/actor.model.ts
@@ -0,0 +1,5 @@
+export interface Actor {
+    name: string,
+    pictureUrl: string,
+    character: string,
+}
--- src/app/features/tvshow-detail/tvshow-detail.component.ts
+++ src/app/features/tvshow-detail/tvshow-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TmdbApiService } from '../../core/services/tmdb-api.service';
 
 @Component({
   selector: 'app-tvshow-detail',
@@ -10,13 +11,45 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TvshowDetailComponent {
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private api: TmdbApiService) {}
 
   id: number | null = 0;
 
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
       this.id = +params.get('id')!;
+    })
+    this.loadData()
+  }
+
+  loadData() {
+    this.fetchData('tv', this.id!);
+    this.fetchCastInfo('tv', this.id!)
+  }
+
+  fetchData(mediaType: string, id: number): void {
+    this.api.getTvShow(mediaType, id).subscribe({
+      next: (response) => {
+        console.log(response)
+      },
+      error: (error) => {
+        console.error('Error fetching configuration data:', error);
+      },
+      complete: () => {
+      }
+    })
+  }
+
+  fetchCastInfo(mediaType: string, id: number): void {
+    this.api.getCredits(mediaType, id).subscribe({
+      next: (response) => {
+        console.log(response)
+      },
+      error: (error) => {
+        console.error('Error fetching configuration data:', error);
+      },
+      complete: () => {
+      }
     })
   }
 
