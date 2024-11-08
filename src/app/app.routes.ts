64c8da31dabfa3fb95c399995a4b4cee05b3fca7
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-11-08 14:43:43 +0100

    [addition] routing for detailed pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { TvshowDetailComponent } from './features/tvshow-detail/tvshow-detail.component';
+import { MovieDetailComponent } from './features/movie-detail/movie-detail.component';
 
 export const routes: Routes = [
     {
@@ -7,5 +9,13 @@ export const routes: Routes = [
       title: 'Application dashboard',
       component: DashboardComponent,
     },
+    {
+      path: '/tvshow/:id',
+      component: TvshowDetailComponent,
+    },
+    {
+      path: '/movie/:id',
+      component: MovieDetailComponent,
+    }
 
 ];
---
+++ src/app/features/tvshow-detail/tvshow-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tvshow-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './tvshow-detail.component.html',
+  styleUrl: './tvshow-detail.component.scss'
+})
+export class TvshowDetailComponent {
+
+}
